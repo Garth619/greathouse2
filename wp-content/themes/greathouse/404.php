0e089335d908b70520a1918d716e6f37d0e5512b
Garrett Cullen <garrettwcullen@gmail.com>
2016-10-26 14:35:10 -0700

    push

--- wp-content/themes/greathouse/404.php
+++ wp-content/themes/greathouse/404.php
@@ -9,22 +9,30 @@
 
 get_header(); ?>
 
-	<div id="container">
-		<div id="content" role="main">
-
-			<div id="post-0" class="post error404 not-found">
-				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
+<div class="container_wrapper">
+	
+	<div class="container">
+		
+		<div id="content">		
+						
+				<h1>Page Not Found</h1>
+						
+					<?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
 					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</div><!-- #post-0 -->
-
-		</div><!-- #content -->
-	</div><!-- #container -->
+				
+	
+	
 	<script type="text/javascript">
 		// focus on search field after it has loaded
 		document.getElementById('s') && document.getElementById('s').focus();
 	</script>
+	
+	</div><!-- content -->
+		
+	</div><!-- container -->
+	
+	<?php get_sidebar(); ?>
+	
+</div><!-- container_wrapper -->
 
 <?php get_footer(); ?>
--- wp-content/themes/greathouse/search.php
+++ wp-content/themes/greathouse/search.php
@@ -9,11 +9,14 @@
 
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+	<div class="container_wrapper">
+	
+		<div class="container">
+		
+			<div id="content">	
 
-<?php if ( have_posts() ) : ?>
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<?php if ( have_posts() ) : ?>
+					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				<?php
 				/*
 				 * Run the loop for the search to output the results.
@@ -22,17 +25,19 @@ get_header(); ?>
 				 */
 				get_template_part( 'loop', 'search' );
 				?>
-<?php else : ?>
-				<div id="post-0" class="post no-results not-found">
-					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-0 -->
-<?php endif; ?>
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+				<?php else : ?>
+					<div id="post-0" class="post no-results not-found">
+						<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
+						<div class="entry-content">
+							<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
+							<?php get_search_form(); ?>
+						</div><!-- .entry-content -->
+					</div><!-- #post-0 -->
+				<?php endif; ?>
+			</div><!-- content -->
+		
+	</div><!-- container -->
+	
+	<?php get_sidebar(); ?>
+	
+</div><!-- container_wrapper --><?php get_footer(); ?>
