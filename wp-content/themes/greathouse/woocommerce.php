480edb02398c4bb5f74e7807d874d735ef3fe86d
Garrett Cullen <garrettwcullen@gmail.com>
2016-11-16 15:49:00 -0800

    push

--- wp-content/themes/greathouse/woocommerce.php
+++ wp-content/themes/greathouse/woocommerce.php
@@ -9,7 +9,74 @@ get_header(); ?>
 
 <?php include('banner.php');?>	
 		
+<?php if(is_product_category()):?>
 
+	<?php 
+		
+		$titleterm_id = get_queried_object()->term_id;
+		$titlepost_id = 'product_cat_'.$titleterm_id;
+	
+	?>
+	
+	
+		
+
+
+<?php if(get_field('sub_product_directory', $titlepost_id)): ?>
+ 
+	<?php while(has_sub_field('sub_product_directory', $titlepost_id)): ?>
+ 		
+ 		
+ 		<?php 
+				$queried_object = get_queried_object(); 
+				$taxonomy = $queried_object->taxonomy;
+				$term_id = $queried_object->term_id;  
+				$related_terms = get_sub_field('link', $taxonomy . '_' . $term_id);
+			?>
+	
+
+ 		
+ 		
+ 		<div class="product_inner">
+	 		
+	 		<div class="single_product_square">
+		 	
+	 			<a href="<?php echo get_term_link($related_terms); ?>">
+		 			
+		 			
+		 			
+		 			<div class="product_overlay">
+				
+						<div class="product_inner_wrapper">
+					
+							<span class="large_header"><?php the_sub_field('category_title', $titlepost_id);?></span><!-- large_header -->
+							<span class="sub_header">shop now</span><!-- sub_header -->
+					
+						</div><!-- product_inner_wrapper -->
+				
+					</div><!-- product_overlay -->
+				
+				
+					<?php $allnewproducts = wp_get_attachment_image_src(get_sub_field('image', $titlepost_id), 'allproducts'); ?>
+  	    
+					<div class="product_background" style="background: rgba(0, 0, 0, 0) url(<?php echo $allnewproducts[0]; ?>) no-repeat scroll left top / cover ;">
+						
+					</div><!-- product_background -->
+		 			
+		 			
+	 			</a>
+	 		
+	 		</div><!-- single_product_square -->
+	 	
+ 		</div><!-- product_inner -->
+  	  	
+	<?php endwhile; ?>
+ 
+<?php endif; ?>
+		
+
+	
+<?php endif;?>
 
 <div class="outer_wrapper">
 
@@ -56,7 +123,7 @@ $filterreset = strtok($filterreset, '?'); ?>
 
 		<div class="product_wrapper"> 
 			
-<!-- 			<?php woocommerce_breadcrumb(); ?> -->
+
 	
 			<?php woocommerce_content(); ?>
 	
