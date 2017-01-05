2456d68f08fa9119cf9495cf9e4a807285fe493d
Garrett Cullen <garrettwcullen@gmail.com>
2017-01-05 15:43:41 -0800

    push

---
+++ wp-content/themes/greathouse/page-designbios.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Design Bios
+ 
+ */
+
+get_header(); ?>
+
+
+<?php include('banner.php');?>
+
+
+<div class="no_sidebar_wrapper">
+	
+	<div class="no_sidebar_spacer"></div><!-- no_sidebar_spacer -->
+
+	<div class="no_sidebar_inner">
+	
+		<span class="large_header_no_sidebar"><?php the_title();?></span><!-- large_header -->
+		
+		<div class="no_sidebar_content">
+	
+			<div id="content">
+			
+				
+					<?php get_template_part( 'loop', 'page' ); ?>
+				
+								
+			</div><!-- content -->
+	
+		</div><!-- no_sidebar_content -->
+	
+	</div><!-- no_sidebar_inner -->
+	
+	<div class="no_sidebar_spacer"></div><!-- no_sidebar_spacer -->
+		
+</div><!-- no_sidebar_wrapper -->
+
+		
+<?php get_footer(); ?>
+
+
+
+
