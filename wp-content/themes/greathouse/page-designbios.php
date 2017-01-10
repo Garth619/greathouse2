2df74a0699a3d5ca8d209f0182a8aa0523c9df13
Garrett Cullen <garrettwcullen@gmail.com>
2017-01-10 10:55:14 -0800

    push

--- wp-content/themes/greathouse/page-designbios.php
+++ wp-content/themes/greathouse/page-designbios.php
@@ -20,14 +20,89 @@ get_header(); ?>
 		
 		<div class="no_sidebar_content">
 	
-			<div id="content">
-			
-				
-					<?php // get_template_part( 'loop', 'page' ); ?>
+			<?php // get_template_part( 'loop', 'page' ); ?>
 					
 					<span class="designer_title">designer</span><!-- designer_title -->
 					
-					<div class="profile_pic"></div><!-- profile_pic -->
+					<div class="profile_pic">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/sandi.jpg"/>
+						
+					</div><!-- profile_pic -->
+					
+					<div id="content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus neque bibendum est vehicula, ut pulvinar orci scelerisque. Fusce ac dolor quis lorem feugiat sollicitudin. Quisque hendrerit molestie volutpat. Donec semper aliquam tempor. In placerat sapien in metus mattis, quis pulvinar orci porta. Sed leo ante, vehicula et nunc in, tristique pharetra ex. In porta quis mi a fringilla. Etiam nec enim feugiat nibh congue iaculis at non ligula. Vivamus at odio et eros convallis feugiat at eget enim. Mauris vitae massa eu quam pellentesque sagittis. Mauris sed commodo arcu. Maecenas malesuada laoreet risus. Sed nec venenatis nisl, non porttitor risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus neque bibendum est vehicula, ut pulvinar orci scelerisque. Fusce ac dolor quis lorem feugiat sollicitudin.</p>
+					
+					<h2>Lorem Ipsum</h2>
+					
+					<p>Quisque hendrerit molestie volutpat. Donec semper aliquam tempor. In placerat sapien in metus mattis, quis pulvinar orci porta. Sed leo ante, vehicula et nunc in, tristique pharetra ex. In porta quis mi a fringilla. Etiam nec enim feugiat nibh congue iaculis at non ligula. Vivamus at odio et eros convallis feugiat at eget enim. Mauris vitae massa eu quam pellentesque sagittis. Mauris sed commodo arcu. Maecenas malesuada laoreet risus. Sed nec venenatis nisl, non porttitor risus:</p>
+					
+					<ul>
+						<li>Quisque hendrerit molestie volutpat</li>
+						<li>Quisque hendrerit molestie volutpat</li>
+						<li>Quisque hendrerit molestie volutpat</li>
+						<li>Quisque hendrerit molestie volutpat</li>
+					</ul>
+					
+					<div class="design_projects">
+						
+						<h1>Design Projects</h1>
+						
+						<span class="designer_title">before + afters</span>
+						
+						
+						<div class="before_after_wrapper">
+						
+								<div class="single">
+								
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+								
+								</div><!-- single -->
+								
+								<h2>Name of Project</h2>
+								
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus neque bibendum est vehicula, ut pulvinar orci scelerisque. Fusce ac dolor quis lorem feugiat sollicitudin. Quisque hendrerit molestie volutpat. Donec semper aliquam tempor. In placerat sapien in metus mattis, quis pulvinar orci porta. Sed leo ante, vehicula et nunc in, tristique pharetra ex. In porta quis mi a fringilla. Etiam nec enim feugiat nibh congue iaculis at non ligula. Vivamus at odio et eros convallis feugiat at eget enim.</p>							
+								
+
+								
+							</div><!-- before_after_wrapper -->
+							
+							<div class="before_after_wrapper">
+						
+								<div class="single">
+								
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+								
+								</div><!-- single -->
+								
+								<h2>Name of Project</h2>
+								
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus neque bibendum est vehicula, ut pulvinar orci scelerisque. Fusce ac dolor quis lorem feugiat sollicitudin. Quisque hendrerit molestie volutpat. Donec semper aliquam tempor. In placerat sapien in metus mattis, quis pulvinar orci porta. Sed leo ante, vehicula et nunc in, tristique pharetra ex. In porta quis mi a fringilla. Etiam nec enim feugiat nibh congue iaculis at non ligula. Vivamus at odio et eros convallis feugiat at eget enim.</p>							
+								
+
+								
+							</div><!-- before_after_wrapper -->
+							
+							<div class="before_after_wrapper">
+						
+								<div class="single">
+								
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+									<img src="<?php bloginfo('template_directory');?>/images/before_1.jpg"/>
+								
+								</div><!-- single -->
+								
+								<h2>Name of Project</h2>
+								
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus neque bibendum est vehicula, ut pulvinar orci scelerisque. Fusce ac dolor quis lorem feugiat sollicitudin. Quisque hendrerit molestie volutpat. Donec semper aliquam tempor. In placerat sapien in metus mattis, quis pulvinar orci porta. Sed leo ante, vehicula et nunc in, tristique pharetra ex. In porta quis mi a fringilla. Etiam nec enim feugiat nibh congue iaculis at non ligula. Vivamus at odio et eros convallis feugiat at eget enim.</p>							
+								
+
+								
+							</div><!-- before_after_wrapper -->
+						
+					</div><!-- design_projects -->
 				
 								
 			</div><!-- content -->
