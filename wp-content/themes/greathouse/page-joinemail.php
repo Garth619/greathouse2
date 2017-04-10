7f24a9de7710896c35b0ea0dfdf1afce9899a463
Garrett Cullen <garrettwcullen@gmail.com>
2017-04-10 09:20:35 -0700

    push

---
+++ wp-content/themes/greathouse/page-joinemail.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Join Email List
+ *
+
+ */
+
+get_header(); ?>
+
+		<?php include('banner.php');?>
+		
+		
+	
+		
+
+		<div class="container_wrapper">
+	
+	<div class="container">
+		
+		<div id="content">
+			
+			<?php get_template_part( 'loop', 'page' ); ?>
+			
+		</div><!-- content -->
+		
+	</div><!-- container -->
+	
+	<?php get_sidebar(); ?>
+	
+</div><!-- container_wrapper -->
+
+
+		
+<?php get_footer(); ?>
